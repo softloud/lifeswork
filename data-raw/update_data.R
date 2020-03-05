96f60c3961223b06fae958f91b1903cf6875564d
Charles T. Gray <charlestigray@gmail.com>
2020-03-05 17:55:56 +1100

    reboot

---
+++ data-raw/update_data.R
@@ -0,0 +1,28 @@
+# update data
+
+library(googlesheets)
+library(tidyverse)
+
+measures <- gs_key("1hv7pkBGu8XQQOIBbBt1_1LvKGBR7zTdQYCzogrv3hz0")
+
+instantiate <- measures %>% 
+  gs_read("instantiate") 
+
+write_rds(instantiate, "data/instantiate.rds")
+
+tasks <- measures %>% gs_read("daily_tasks") %>% 
+  mutate(
+    p = priority,
+    p = fct_relevel(p, "star", "sim", "varnothing"),
+    c = category,
+    c = fct_relevel(c, "phi", "theta", "psi", "pi"),
+    priority = paste0("$\\", priority, "$"),
+    category = paste0("$\\", category, "$")) %>% 
+  arrange(c,p)
+  
+
+write_rds(tasks, "data/tasks.rds")
+
+review <- measures %>% gs_read("review")
+
+write_rds(review, "data/review.rds")
